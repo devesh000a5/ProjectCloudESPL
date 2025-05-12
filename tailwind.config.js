8b13866e857ef2bb354ff378a8fc7b2868a804e9
Devesh Singh <deveshsingh23503@gmail.com>
2025-05-12 19:14:25 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  
+  theme: {
+    extend: {
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
